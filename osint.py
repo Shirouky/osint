97d3727dbd1e8ee1653f98422fd2ec558c70d149
Luba Derebas <luba_d2005@mail.ru>
2022-06-05 13:17:05 +0300

    edit

--- osint.py
+++ osint.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import re
 
 soup_pages = []
-# name, surname = input().split()
-name, surname = 'александр чумилин'.split()
+name, surname = input().split()
+
 for i in range(3):
     url = f'https://yandex.ru/search/?text="{name}"+"{surname}"&lr=101718&clid=2285101&p={i}'
     resp = requests.get(url)
@@ -14,7 +14,6 @@ for i in range(3):
 links = []
 for soup_page in soup_pages:
     for a in soup_page.find_all('a'):
-        print(a['href'])
         if 'href' in a.attrs and 'google' not in a['href'] and 'yandex' not in a['href'] and 'search' not in a[
             'href'] and a['href'] not in links:
             links.append(a['href'])
